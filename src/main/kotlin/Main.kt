cdcc4c2267fd0ea0bc2e450c0227e5da0596e5b3
Artem <79054301+Bumer-32@users.noreply.github.com>
2025-06-27 23:43:07 +0300

    add logs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 package ua.pp.lumivoid
 
+import org.slf4j.LoggerFactory
+
+
 fun main() {
+    val logger = LoggerFactory.getLogger("main")
+
+    logger.trace("This is trace log")
+    logger.debug("This is debug log")
+    logger.info("This is info log")
+    logger.warn("This is warn log")
+    logger.error("This is error log")
 }
